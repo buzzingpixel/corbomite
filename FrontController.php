77af31ca40ad985f040b8d0926f5fd84ea4c94e1
TJ Draper <tj@buzzingpixel.com>
2018-12-29 17:44:04 -0600

    Install corbomite cli and set it up in the front controller

--- FrontController.php
+++ FrontController.php
@@ -3,20 +3,23 @@ declare(strict_types=1);
 
 // phpcs:ignoreFile
 
-$sep = DIRECTORY_SEPARATOR;
+use corbomite\di\Di;
+use corbomite\cli\Kernel;
+
 define('APP_BASE_PATH', __DIR__);
-define('APP_VENDOR_PATH', APP_BASE_PATH . $sep . 'vendor');
+define('APP_VENDOR_PATH', APP_BASE_PATH . '/vendor');
 
-require APP_VENDOR_PATH . $sep . 'autoload.php';
+require APP_VENDOR_PATH . '/autoload.php';
 
-if (file_exists(APP_BASE_PATH . $sep . '.env')) {
+if (file_exists(APP_BASE_PATH . '/.env')) {
     (new Dotenv\Dotenv(APP_BASE_PATH))->load();
 }
 
 if (PHP_SAPI === 'cli') {
-    require APP_BASE_PATH . $sep . 'FrontControllerCli.php';
+    /** @noinspection PhpUnhandledExceptionInspection */
+    Di::get(Kernel::class)($argv);
     exit();
 }
 
-require APP_BASE_PATH . $sep . 'FrontControllerHttp.php';
+require APP_BASE_PATH . '/FrontControllerHttp.php';
 exit();
--- FrontControllerCli.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-
-// TODO: Build out CLI front controller
-var_dump('TODO: Build out CLI front controller');
