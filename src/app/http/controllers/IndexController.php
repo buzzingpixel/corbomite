36c68cb5f11bfa3233e9673b1b32cbc1f8602715
TJ Draper <tj@buzzingpixel.com>
2018-12-30 15:47:35 -0600

    Add corbomite twig environment

--- src/app/http/controllers/IndexController.php
+++ src/app/http/controllers/IndexController.php
@@ -3,22 +3,31 @@ declare(strict_types=1);
 
 namespace src\app\http\controllers;
 
+use corbomite\twig\TwigEnvironment;
 use Psr\Http\Message\ResponseInterface;
 
 class IndexController
 {
     private $response;
+    private $twigEnvironment;
 
-    public function __construct(ResponseInterface $response)
-    {
+    public function __construct(
+        ResponseInterface $response,
+        TwigEnvironment $twigEnvironment
+    ) {
         $this->response = $response;
+        $this->twigEnvironment = $twigEnvironment;
     }
 
     public function __invoke(): ResponseInterface
     {
         $response = $this->response->withHeader('Content-Type', 'text/html');
 
-        $response->getBody()->write('TODO: Build out index page');
+        $response->getBody()->write(
+            $this->twigEnvironment->render('Index.twig', [
+                'someVar' => 'someVal',
+            ])
+        );
 
         return $response;
     }
