540e7102cf66fa1470027a5387fbcf074cb0f156
TJ Draper <tj@buzzingpixel.com>
2019-01-10 23:35:24 -0600

    Add Atlas generated data files

--- FrontController.php
+++ FrontController.php
@@ -10,6 +10,8 @@ use corbomite\http\Kernel as HttpKernel;
 define('APP_BASE_PATH', __DIR__);
 define('APP_VENDOR_PATH', APP_BASE_PATH . '/vendor');
 putenv('TWIG_CACHE_PATH=' . APP_BASE_PATH . '/cache/twig');
+putenv('CORBOMITE_DB_DATA_NAMESPACE=src\app\data');
+putenv('CORBOMITE_DB_DATA_DIRECTORY=./src/app/data');
 
 require APP_VENDOR_PATH . '/autoload.php';
 
