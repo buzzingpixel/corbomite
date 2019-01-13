6cafbc731344f2479eb0ccdd77cb76d19e8321b3
TJ Draper <tj@buzzingpixel.com>
2019-01-12 19:23:17 -0600

    Set up basic Twig Templating scheme

--- src/config/http/globals.php
+++ src/config/http/globals.php
@@ -3,4 +3,7 @@ declare(strict_types=1);
 
 return [
     'devMode' => getenv('DEV_MODE') === 'true',
+    'siteName' => 'BuzzingPixel',
+    'siteTwitterHandle' => '',
+    'staticAssetCacheTime' => '',
 ];
